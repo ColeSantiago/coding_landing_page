a7a93bec90816879bb18ac940207eec5a5828a82
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-06-09 11:04:52 -0400

    fixed the positioning for the iphone image, added a slideshow for the mobile view for the first section, and fixed the positioning of the form for the mobile view

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  	let acc = document.getElementsByClassName("accordion");
+$(document).ready(function(event) {
+	// accordion controls 
+  	let acc = $(".accordion");
 	let i;
 
 	for (i = 0; i < acc.length; i++) {
@@ -13,4 +14,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	    	} 
 	  	});
 	}
+
+	// slideshow controls
+	let slideIndex = 1;
+	showSlides(slideIndex);
+
+	// Next/previous controls
+	function plusSlides(n) {
+	  	showSlides(slideIndex += n);
+	};
+
+	// Thumbnail image controls
+	function currentSlide(n) {
+	  	showSlides(slideIndex = n);
+	};
+
+	function showSlides(n) {
+	  	let i;
+	  	let slides = $(".mySlides");
+	  	let dots = $(".dot");
+
+	  	if (n > slides.length) {slideIndex = 1} 
+	  	if (n < 1) {slideIndex = slides.length}
+	  	for (i = 0; i < slides.length; i++) {
+	      	slides[i].style.display = "none"; 
+	  	}
+	  	for (i = 0; i < dots.length; i++) {
+	     	dots[i].className = dots[i].className.replace(" active", "");
+	  	}
+	  	slides[slideIndex-1].style.display = "block"; 
+	};
+
+	$(".prev").on("click", function() {
+		plusSlides(-1);
+	});
+
+	$(".next").on("click", function() {
+		plusSlides(1);
+	});
+
 });
